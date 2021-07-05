c1fede308cb49021030c9922609eeb36aa85757f
Camilo A <ing_camiloacg@hotmail.com>
2021-07-05 08:56:56 -0500

    Se crea digrama de clases y proyecto de la tienda

---
+++ tienda/src/Producto.java
@@ -0,0 +1,10 @@
+public class Producto {
+
+    private String nombre = "";
+    private float precio = 0;
+
+    public void mostrar() {
+        System.out.println(nombre + ": " + precio);
+    }
+
+}
