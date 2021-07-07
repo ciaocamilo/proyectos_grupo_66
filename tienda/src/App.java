0cc8be444a2064eccbae14863442147b1927a14b
Camilo A <ing_camiloacg@hotmail.com>
2021-07-07 08:17:56 -0500

    Se crea clase Producto con metodos acesorios  y su respectivo objeto

--- tienda/src/App.java
+++ tienda/src/App.java
@@ -1,5 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("La Tienda de Ropa");
+
+        Producto productoUno = new Producto();
+
+        productoUno.setNombre("Falda");
+        productoUno.setPrecio(70000);
+        productoUno.mostrar();
+
     }
 }
--- tienda/src/Producto.java
+++ tienda/src/Producto.java
@@ -3,8 +3,29 @@ public class Producto {
     private String nombre = "";
     private float precio = 0;
 
+    public Producto() {
+
+    }
+
     public void mostrar() {
-        System.out.println(nombre + ": " + precio);
+        String precioTexto = String.format("%,d", (int)precio);
+        System.out.println(nombre + ": " + precioTexto);
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public float getPrecio() {
+        return this.precio;
     }
 
 }
