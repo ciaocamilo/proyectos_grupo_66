8b66fdf221b788186150718c8d4e3ba0344b4ba8
Camilo A <ing_camiloacg@hotmail.com>
2021-07-07 08:49:06 -0500

    Se cambia mensaje de la tienda

--- tienda/src/App.java
+++ tienda/src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("La Tienda de Ropa");
+        System.out.println("*La Tienda de Ropa**");
 
         Producto productoUno = new Producto();
 
