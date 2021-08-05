e2b41463002d8e2e1a25787a8814d0d38f57bcf2
Camilo A <ing_camiloacg@hotmail.com>
2021-08-05 07:02:59 -0500

    Agrega material JAVA -  JDBC Sqlite y clase conexión

---
+++ Material JAVA/SqliteConnection.java
@@ -0,0 +1,51 @@
+package conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class SqliteConnection {
+
+    private Connection conn;
+
+    public SqliteConnection () {
+
+        String archivoDB = "base_de_datos/tienda.db"; // Debe contener la ubicación y nombre del archivo .db
+        String url = "jdbc:sqlite:" + archivoDB;
+        conn = null;
+
+        try {
+            conn = DriverManager.getConnection(url);
+            System.out.println("..Conectado a la Base de datos");
+        } catch (SQLException e) {
+            System.err.println(e);
+        }
+
+    }
+
+    public ResultSet ejecutarConsulta(String consulta) {
+        ResultSet rs = null;
+        try {
+            Statement stmt = conn.createStatement();
+            rs = stmt.executeQuery(consulta);
+        } catch (SQLException e) {
+            System.err.println(e);
+        }
+        return rs;
+    }
+
+    public int ejecutarSentencia(String sentencia) {
+        int nFilas = 0;
+        try {
+            PreparedStatement pstmt = conn.prepareStatement(sentencia);
+            nFilas = pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.err.println(e);
+        }
+        return nFilas;
+    }
+
+}
