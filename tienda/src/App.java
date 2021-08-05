5f43ed5151a98448b60aca7c9e804d95f523ac3b
Camilo A <ing_camiloacg@hotmail.com>
2021-08-05 08:54:00 -0500

    Organiza en paquetes proyecto Tienda y crea conexión a la base de datos

--- tienda/src/App.java
+++ tienda/src/App.java
@@ -1,12 +1,17 @@
+import modelo.*;
+import conexion.SqliteConnection;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("*La Tienda de Ropa**");
 
-        Producto productoUno = new Producto();
+        SqliteConnection conexionBD = new SqliteConnection();
 
-        productoUno.setNombre("Falda");
-        productoUno.setPrecio(70000);
-        productoUno.mostrar();
+        // Producto productoUno = new Producto();
+
+        // productoUno.setNombre("Falda");
+        // productoUno.setPrecio(70000);
+        // productoUno.mostrar();
 
     }
 }
---
+++ tienda/src/modelo/Producto.java
@@ -0,0 +1,33 @@
+package modelo;
+
+public class Producto {
+
+    private String nombre = "";
+    private float precio = 0;
+
+    public Producto() {
+
+    }
+
+    public void mostrar() {
+        String precioTexto = String.format("%,d", (int)precio);
+        System.out.println(nombre + ": " + precioTexto);
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public float getPrecio() {
+        return this.precio;
+    }
+
+}
